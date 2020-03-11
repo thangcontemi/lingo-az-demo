dda99fcbd4cfe71943ec920283b9c7562a0667c8
kata <katawo@gmail.com>
2020-03-11 16:52:58 +0700

    try with failed test case

--- Hello/__tests__/index.js
+++ Hello/__tests__/index.js
@@ -4,7 +4,7 @@ test('a trivial test case', () => {
   expect(true).toBe(true);
 });
 
-test('', async () => {
+test('passed case', async () => {
   const context = {};
   const req = {
     query: {
@@ -14,4 +14,16 @@ test('', async () => {
 
   await helloFunction(context, req);
   expect(context.res.body).toMatch(/Hello, lingo/);
+});
+
+test('failed case', async () => {
+  const context = {};
+  const req = {
+    query: {
+      name: 'lingo'
+    }
+  };
+
+  await helloFunction(context, req);
+  expect(context.res.body).toMatch(/Hello, lingo1/);
 });
